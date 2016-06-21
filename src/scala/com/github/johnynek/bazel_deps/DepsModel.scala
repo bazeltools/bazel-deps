5a2ddba124603afd254236e93e75d149486e24ab
Oscar Boykin <oscar@stripe.com>
2016-06-21 11:07:53 -1000

    improve version sorting and tests

--- src/scala/com/github/johnynek/bazel_deps/DepsModel.scala
+++ src/scala/com/github/johnynek/bazel_deps/DepsModel.scala
@@ -19,8 +19,8 @@ case class Model(
 object Model {
   def readFile(f: File): Try[String] = Try {
     val fr = new FileReader(f)
-    val buf = new BufferedReader(fr)
     try {
+      val buf = new BufferedReader(fr)
       val bldr = new java.lang.StringBuilder
       val cbuf = new Array[Char](1024)
       var read = 0
@@ -47,21 +47,80 @@ case class Sha1Value(toHex: String)
 case class MavenServer(id: String, contentType: String, url: String)
 
 object Version {
-  implicit def versionOrdering: Ordering[Version] = {
-    val strNumOrd: Ordering[String] = new Ordering[String] {
-      def compare(left: String, right: String): Int = {
-        def toLong(s: String): Either[String, Long] =
-          Try(s.toLong).toOption.fold(Left(s): Either[String, Long])(Right(_))
+  private val isNum: Set[Char] = Set('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+  /**
+   * break a string into alternating runs of Longs and Strings
+   */
+  private def tokenize(s: String): List[Either[String, Long]] = {
+    def append(a: List[Either[String, Long]], b: Either[List[Char], List[Char]]): List[Either[String, Long]] =
+      b match {
+        case Right(thisAcc) =>
+          Right(thisAcc.reverse.mkString.toLong) :: a
+        case Left(thisAcc) =>
+          Left(thisAcc.reverse.mkString) :: a
+      }
 
-        (toLong(left), toLong(right)) match {
+    val (acc, toAdd) =
+      s.foldLeft((List.empty[Either[String, Long]], Option.empty[Either[List[Char], List[Char]]])) {
+        // Here are the first characters
+        case ((acc, None), c) if isNum(c) =>
+          (acc, Some(Right(c :: Nil)))
+        case ((acc, None), c) if !isNum(c) =>
+          (acc, Some(Left(c :: Nil)))
+        // Here we continue with the same type
+        case ((acc, Some(Right(thisAcc))), c) if isNum(c) =>
+          (acc, Some(Right(c :: thisAcc)))
+        case ((acc, Some(Left(thisAcc))), c) if !isNum(c)=>
+          (acc, Some(Left(c :: thisAcc)))
+        // Here we switch type and add to the acc
+        case ((acc, Some(r@Right(thisAcc))), c) if !isNum(c)=>
+          (append(acc, r), Some(Left(c :: Nil)))
+        case ((acc, Some(l@Left(thisAcc))), c) if isNum(c) =>
+          (append(acc, l), Some(Right(c :: Nil)))
+      }
+    toAdd.fold(acc)(append(acc, _)).reverse
+  }
+
+  implicit def versionOrdering: Ordering[Version] = {
+    implicit val strNumOrd: Ordering[Either[String, Long]] = new Ordering[Either[String, Long]] {
+      def compare(left: Either[String, Long], right: Either[String, Long]): Int = {
+        (left, right) match {
           case (Right(a), Right(b)) => java.lang.Long.compare(a, b)
-          case (Right(_), Left(_)) => 1 // put non number before number
+          case (Right(_), Left(_)) => 1 // put non number before number (eg, "-RC" comes before 2)
           case (Left(_), Right(_)) => -1
           case (Left(a), Left(b)) => a.compareTo(b)
+            val commonTokens = Set("alpha", "beta", "pre", "rc", "m")
+            val al = a.toLowerCase
+            val bl = b.toLowerCase
+            if (commonTokens(al) && commonTokens(bl)) {
+              al.compareTo(bl)
+            } else a.compareTo(b)
         }
       }
     }
-    Ordering.by { v: Version => v.asString.split('.').toIterable }(Ordering.Iterable(strNumOrd))
+    // In versions, if one is a prefix of the other, and the next item is
+    // not a number, it is bigger.
+    @annotation.tailrec
+    def prefixCompare[T: Ordering](a: List[T], b: List[T])(fn: T => Int): Int = (a, b) match {
+      case (Nil, h :: tail) => fn(h)
+      case (h :: tail, Nil) => -fn(h)
+      case (Nil, Nil) => 0
+      case (ha :: taila, hb :: tailb) =>
+        val c = Ordering[T].compare(ha, hb)
+        if (c == 0) prefixCompare(taila, tailb)(fn)
+        else c
+    }
+    Ordering.by { v: Version =>
+      v.asString.split("\\.|\\-") // note this is a regex
+        .flatMap(tokenize)
+        .toList
+    }(new Ordering[List[Either[String, Long]]] {
+      def compare(a: List[Either[String, Long]], b: List[Either[String, Long]]) =
+        prefixCompare(a, b) {
+          case Left(_) => 1 // if see a string, the shorter one is larger
+          case Right(_) => -1 // if we see a number, the shorter is smaller
+        }
+    })
   }
 }
 
--- test/scala/com/github/johnynek/bazel_deps/ModelTest.scala
+++ test/scala/com/github/johnynek/bazel_deps/ModelTest.scala
@@ -10,6 +10,21 @@ class ModelTest extends FunSuite {
 
     assert(VersionConflictPolicy
       .Highest
-      .resolve(Some(Version("18.0")), Set(Version("11.0.2"))) === Right(Version("18.0")))
+      .resolve(Some(Version("18.1")), Set(Version("11.0.2"))) === Right(Version("18.1")))
+  }
+  test("RC/M is less than final") {
+    val ord = Ordering[Version]
+    assert(ord.lt(Version("0.5.0-M2"), Version("0.5.0")), "0.5.0-M2 < 0.5.0")
+    assert(ord.lt(Version("0.16.1-RC2"), Version("0.16.1")), "0.16.1-RC1 < 0.16.1")
+    assert(ord.lt(Version("0.16.1-RC2"), Version("0.16.10-RC4")), "0.16.1-RC1 < 0.16.10-RC4")
+    assert(ord.lt(Version("0.16.2-RC2"), Version("0.16.10-RC4")), "0.16.2-RC1 < 0.16.10-RC4")
+  }
+  test("a test array is sorted") {
+    val sorted =
+      List("1.0.9a", "1.0.9", "1.0.10", "2.0RC0", "2.0-rc1", "2.0rc2", "2.0", "3.1.4.2-M1", "3.1.4.2", "10.2")
+        .map(Version(_))
+
+    val rand = scala.util.Random.shuffle(sorted)
+    assert(rand.sorted === sorted)
   }
 }
