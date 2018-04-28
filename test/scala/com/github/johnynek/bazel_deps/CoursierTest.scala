5650dd780666c2c6efa03da07d23d52d15394fb2
Robert Gay <robert.gay@redfin.com>
2018-04-28 18:40:10 -0500

    Adding jar- and classifier-exclude Coursier tests

--- test/scala/com/github/johnynek/bazel_deps/CoursierTest.scala
+++ test/scala/com/github/johnynek/bazel_deps/CoursierTest.scala
@@ -93,4 +93,71 @@ dependencies:
       MavenCoordinate(
         MavenGroup("org.sonatype.sisu"), MavenArtifactId("sisu-guice", "jar", "no_aop"), Version("2.9.4"))))
   }
+
+  test("test non-jar packaging excludes example") {
+    val config = """
+options:
+  buildHeader: [ "load(\"@io_bazel_rules_scala//scala:scala_import.bzl\", \"scala_import\")" ]
+  languages: [ "java", "scala:2.11.8" ]
+  resolverType: "coursier"
+  resolvers:
+    - id: "mavencentral"
+      type: "default"
+      url: https://repo.maven.apache.org/maven2/
+  transitivity: runtime_deps
+  versionConflictPolicy: highest
+dependencies:
+  com.amazonaws:
+    aws-dynamodb-encryption-java:
+      lang: java
+      version: "1.11.0"
+      exclude:
+        - "com.almworks.sqlite4java:libsqlite4java-linux-amd64:so"
+"""
+
+    val model = Decoders.decodeModel(Yaml, config).right.get
+    val (graph, shas, normalized) = MakeDeps.runResolve(model, null).get
+
+    assert(graph.nodes.contains(
+      MavenCoordinate(
+        MavenGroup("com.amazonaws"), MavenArtifactId("aws-dynamodb-encryption-java"), Version("1.11.0"))))
+
+    // make sure no non-jar packaging shows up, until bazel-deps can handle setting those
+    // as data dependencies
+    val nonJarNodes = graph.nodes
+        .filter { mc => mc.artifact.packaging != "jar" }
+
+    assert(nonJarNodes.size == 0)
+  }
+
+  test("test jar packaging w/ classifier exclude example") {
+    val config =
+      """
+options:
+  buildHeader: [ "load(\"@io_bazel_rules_scala//scala:scala_import.bzl\", \"scala_import\")" ]
+  languages: [ "java", "scala:2.11.8" ]
+  resolverType: "coursier"
+  resolvers:
+    - id: "mavencentral"
+      type: "default"
+      url: https://repo.maven.apache.org/maven2/
+  transitivity: runtime_deps
+  versionConflictPolicy: highest
+dependencies:
+  org.sonatype.sisu:
+    sisu-inject-bean:
+      lang: java
+      version: "2.1.1"
+      exclude:
+        - "org.sonatype.sisu:sisu-guice:jar:no-aop"
+"""
+
+    val model = Decoders.decodeModel(Yaml, config).right.get
+    val (graph, shas, normalized) = MakeDeps.runResolve(model, null).get
+    // org.sonatype.sisu:sisu-guice:no_aop is a transitive dependency of org.sonatype.sisu:sisu-inject-bean
+    // we exclude it for this test
+    assert(!graph.nodes.contains(
+      MavenCoordinate(
+        MavenGroup("org.sonatype.sisu"), MavenArtifactId("sisu-guice", "jar", "no_aop"), Version("2.9.4"))))
+  }
 }
