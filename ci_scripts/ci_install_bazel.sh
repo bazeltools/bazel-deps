cc0d0ffa265dc68eb133e66db549a697f0848e82
Ian O'Connell <ianoc@ianoc.net>
2020-03-01 14:47:01 -0700

    cp

--- ci_scripts/ci_install_bazel.sh
+++ ci_scripts/ci_install_bazel.sh
@@ -1,11 +1,10 @@
 set -e
-BAZEL_PLATFORM_NAME=$1
 cd $GITHUB_WORKSPACE
 
-wget "https://github.com/bazelbuild/bazel/releases/download/2.1.1/bazel-2.1.1-installer-${BAZEL_PLATFORM_NAME}-x86_64.sh"
-sha256sum -c ci_scripts/.bazel-installer-${BAZEL_PLATFORM_NAME}-x86_64.sh.sha256
-chmod +x bazel-2.1.1-installer-${BAZEL_PLATFORM_NAME}-x86_64.sh
-./bazel-2.1.1-installer-${BAZEL_PLATFORM_NAME}-x86_64.sh --user
+cp ci_scripts/bootstrapping_bazel bazel
+cp ci_scripts/bootstrapping_bazel tools/bazel
+
+./bazel
 cp .bazelrc.travis .bazelrc
 
 
