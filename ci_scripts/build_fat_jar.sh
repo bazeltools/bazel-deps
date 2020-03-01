64fc129fd770201701f297b66c730daf12bce232
Ian O'Connell <ianoc@ianoc.net>
2020-03-01 14:47:40 -0700

    cp

--- ci_scripts/build_fat_jar.sh
+++ ci_scripts/build_fat_jar.sh
@@ -1,3 +1,3 @@
 set -e
-bazel build src/scala/com/github/johnynek/bazel_deps/parseproject_deploy.jar
+./bazel build src/scala/com/github/johnynek/bazel_deps/parseproject_deploy.jar
 cp bazel-bin/src/scala/com/github/johnynek/bazel_deps/parseproject_deploy.jar bazel_deps.jar
